3ce4d01d3c1aa85ca991c93a57d9fb95d091ad36
shubham <shubhshri25gmail.com>
2022-07-31 13:44:36 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("0");
+  const [height, setHeight] = useState("0");
+  const [superPower, setsuperPower] = useState("");
+  const [dislayCharacter, setDisplayCharacter] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Build a Hero</h1>
+      <div>
+        <label>Name: </label>
+        <input
+          type="text"
+          style={{ margin: 10 }}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <label>Age: </label>
+        <input
+          type="number"
+          style={{ margin: 10 }}
+          onChange={(event) => setAge(event.target.value)}
+        />
+        <label>height: </label>
+        <input
+          type="number"
+          style={{ margin: 10 }}
+          onChange={(event) => setHeight(event.target.value)}
+        />
+        <label>superPower: </label>
+        <input
+          type="text"
+          style={{ margin: 10 }}
+          onChange={(event) => setsuperPower(event.target.value)}
+        />
+      </div>
+      <button
+        onClick={() => {
+          setDisplayCharacter(true);
+        }}
+      >
+        Display character
+      </button>
+      <div>
+        <h1>Hero info</h1>
+        {dislayCharacter && (
+          <div>
+            <ul>
+              <li>Nme:{name}</li>
+              <li>Age:{age}</li>
+              <li>Height:{height}</li>
+              <li>superPower:{superPower}</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
